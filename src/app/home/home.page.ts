761c722f633bcd4ee2658d0a0e88e35ffcf57d9c
walaa <walaafathel@yahoo.com>
2019-07-15 21:40:02 +0300

    first HW ionic

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, } from '@angular/core';
+import { RouterModule, Routes} from '@angular/router';
+import { Router } from '@angular/router'; 
+
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,22 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private router: Router) {}
+
+
+  title="تجريبي"
+
+
+  //fuction go to login page
+  jump(){
+    console.log("You ar click button");
+
+    // this roter used by navigate
+    // this.router.navigate(['/login']);
+
+    // this roter used by navigateurl
+    this.router.navigateByUrl('login');
+
+  }
 
 }
